e8f0e0aee34f07b56110de32f63af485c7f53ab3
Andy Dm <demarieandy@outlook.com>
2019-08-05 17:07:09 +0200

    NL Updated

--- phpbbmodders/adduser/language/nl/info_acp_adduser.php
+++ phpbbmodders/adduser/language/nl/info_acp_adduser.php
@@ -42,8 +42,8 @@ $lang = array_merge($lang, array(
 	'ACP_ADD_USER'				=> 'ACP Voeg gebruiker toe',
 	'DIR_NOT_EXIST'				=> 'De gekozen taal %s heeft niet de benodigde bestanden voor de extensie. Gelieve ze te vertalen en upload deze naar de %s folder van de extensie om de extensie te gebruiken.',
 	'ACP_ACCOUNT_ADDED'			=> 'Het gebruikersaccount werd aangemaakt. De gebruiker kan nu aanmelden met de gebruikersnaam en het wachtwoord dat verzonden werd naar het door jou opgegeven e-mail adres.',
-	'ACP_ACCOUNT_INACTIVE'		=> 'Het gebruikersaccount werd aangemaakt. Maar de forum instellingen vereisen dat de gebruiker zijn/haar account activeert.<br />Een activatie sleutel werd verzonden naar het door jou opgegeven e-mail adres van de gebruiker.',
-	'ACP_ACCOUNT_INACTIVE_ADMIN'=> 'Het gebruikersaccount werd aangemaakt. Maar de forum instellingen vereisen account activatie door een beheerder.<br />Er werd een e-mail verzonden naar de beheerders en de gebruiker zal ge&iuml;nformeerd worden wanneer zijn/haar account geactiveerd is.',
+	'ACP_ACCOUNT_INACTIVE'		=> 'Het gebruikersaccount werd aangemaakt. Maar de forum instellingen vereisen dat de gebruiker zijn/haar account activeert.<br>Een activatie sleutel werd verzonden naar het door jou opgegeven e-mail adres van de gebruiker.',
+	'ACP_ACCOUNT_INACTIVE_ADMIN'=> 'Het gebruikersaccount werd aangemaakt. Maar de forum instellingen vereisen account activatie door een beheerder.<br>Er werd een e-mail verzonden naar de beheerders en de gebruiker zal geïnformeerd worden wanneer zijn/haar account geactiveerd is.',
 	'ACP_ADMIN_ACTIVATE'		=> 'Een e-mail wordt verzonden naar een beheerder voor account activering. Als alternatief kunt u het onderstaande account vakje aanvinken om het account onmiddellijk te activeren wanneer u een account aangemaakt hebt. De gebruiker zal een e-mail ontvangen met de account login details.',
 	'ACP_EMAIL_ACTIVATE'		=> 'Zodra het account is aangemaakt, ontvangt de gebruiker een e-mail met een activatie link om het account te activeren.',
 	'ACP_INSTANT_ACTIVATE'		=> 'Het account zal onmiddellijk geactiveerd worden. De gebruiker zal een mail ontvangen met alle benodigde account details.',
@@ -61,5 +61,5 @@ $lang = array_merge($lang, array(
 	'ENABLE_NEWUSER'			=> 'Schakel nieuwe gebruiker in',
 	'ENABLE_NEWUSER_EXPLAIN'	=> 'Indien ingesteld op ja zal de gebruiker toegevoegd worden aan de Onlangs geregistreerde gebruikers groep',
 	// ACP Logs
-	'LOG_USER_ADDED'			=> '<strong>Nieuwe gebruiker aangemaakt</strong><br />» %s',
+	'LOG_USER_ADDED'			=> '<strong>Nieuwe gebruiker aangemaakt</strong><br>» %s',
 ));
